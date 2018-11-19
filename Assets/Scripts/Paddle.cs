a370fe253e2ce56b53eafcb68e76b043809b6e16
Armando <armandocatillo559@gmail.com>
2018-11-19 10:22:50 -0800

    initial push

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float gameWorldWidth = 16f;
+    [SerializeField] float minPos = 0f;
+    [SerializeField] float maxPos = 16f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * gameWorldWidth;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minPos, maxPos);
+        transform.position = paddlePos;
+	}
+}
